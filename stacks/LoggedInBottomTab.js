2357491c76245a294dcb475833bdfad644d8c762
gladyn <gladynwong@gmail.com>
2022-07-24 22:53:07 +0800

    bottom tab done

---
+++ stacks/LoggedInBottomTab.js
@@ -0,0 +1,15 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+import Account from '../screens/Account';
+import MyTripsStack from './MyTripsStack';
+
+const Tab = createBottomTabNavigator();
+
+export default function LoggedInBottomTab({navigation}) {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="My Trips" component={MyTripsStack} />
+      <Tab.Screen name="Account" component={Account} />
+    </Tab.Navigator>
+  );
+}
