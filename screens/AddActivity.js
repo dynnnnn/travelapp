d66c40602ff1c1f0cf6292c16a85020372d250bf
gladyn <gladynwong@gmail.com>
2022-07-26 23:16:02 +0800

    add activity to db

--- screens/AddActivity.js
+++ screens/AddActivity.js
@@ -1,11 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, {useState} from 'react';
 import Title from '../components/Title';
+import firebase from 'firebase';
+
+const db = firebase.firestore();
 
 const AddActivity = ({navigation, route}) => {
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("");
+    const [activity, setActivity] = useState("");
+
+    const id = route.params.id;
+
+    async function submitHandler() {
+        const newActivity = await db
+          .collection("trips")
+          .doc(id)
+          .collection(date)
+          .doc()
+        //   .collection("activity")
+        //   .doc()
+          .set({
+           date: date,
+           time: time,
+           activity: activity
+          });
+    
+        navigation.navigate("tripdetails");
+      }
+
+
   return (
     <View>
       <Title>Add Activity</Title>
+
+      <TextInput placeholder="date" value={date} onChangeText={(text) => {
+          setDate(text);
+        }}/>
+      <TextInput placeholder="time" value={time} onChangeText={(text) => {
+          setTime(text);
+        }}/>
+      <TextInput placeholder="address" value={activity} onChangeText={(text) => {
+          setActivity(text);
+        }}/>
+      <Button title="add" onPress={submitHandler} />
+
     </View>
   )
 }
