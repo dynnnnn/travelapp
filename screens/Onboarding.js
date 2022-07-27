94676b7a10f617e075f8f9fffc4b340343daae0d
maswirasuria <maswirasuria@gmail.com>
2022-07-27 18:59:14 +0800

    Add Onboarding in screen

---
+++ screens/Onboarding.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  Alert
+} from 'react-native';
+import Swiper from 'react-native-swiper';
+
+const { width, height } = Dimensions.get('window');
+
