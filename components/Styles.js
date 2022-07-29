d45ed2d9e25ea344f402b8a85e78f4366fa475f2
maswirasuria <maswirasuria@gmail.com>
2022-07-29 14:04:16 +0800

    Add styles.js

---
+++ components/Styles.js
@@ -0,0 +1,112 @@
+export const Styles = {
+
+ container: {
+     flex: 1,
+     padding: 24,
+     backgroundColor: "#FFFFFF",
+     justifyContent:"center",
+  },
+
+ button: {
+    backgroundColor: '#00266B',
+    borderRadius: 55,
+    width: "50%",
+    alignItems: "center",
+    fontFamily: "BandaNova",
+ },
+
+ buttonText: { 
+    fontSize: 20, 
+    margin: 10,
+    color: "#FFFFFF",
+    fontFamily: "BandaNova",
+    alignItems: "center",
+ },
+
+ title: {
+   fontSize: 50, 
+   textAlign: 'center',
+   fontFamily: "BandaNova",
+   color: '#FFFFFF',  
+ },
+
+ content: {
+   fontSize: 20,
+   fontFamily: "BandaNova",
+ },
+
+  text: {
+   color: "#FFFFFF",
+   fontFamily: "BandaNova",
+   textAlign: "center",
+ },
+
+  textInput:{
+   fontFamily: "BandaNova",
+   fontSize: 20,
+   textAlign:"center",
+   color: '#FFFFFF',
+ } ,
+ 
+  switchText:{
+   fontFamily:"BandaNova",
+   fontSize: 30,
+   textAlign:"center",
+   color: '#FFFFFF',
+ },
+}
+ 
+export const lightStyles = {
+ 
+  text: {
+    color: "#00266B",
+    fontFamily: "BandaNova",
+    fontSize: 30,
+ },
+
+  header: {
+    backgroundColor: "#00266B",
+    height: 100,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+ },
+
+  headerTitle: {
+    fontSize: 40,
+    color: "#FFFFFF",
+   fontFamily: "BandaNova",
+ },
+
+  headerTint: "#FFFFFF",
+};
+
+export const darkStyles = {
+  container: {
+    flex: 1,
+    backgroundColor: "#00266B",
+ },
+
+  text: {
+    color: "#FFFFFF",
+    fontFamily: "BandaNova",
+    fontSize: 30,
+ },
+
+  header: {
+    backgroundColor: "#00266B",
+    height: 100,
+    shadowColor: "#FFFFFF",
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+ },
+
+  headerTitle: {
+    fontSize: 40,
+    fontFamily:"Aldo-Pro",
+    alignItems: "center",
+    color: "white"
+  },
+  
+ headerTint: "#00266B",
+};
