dce9b7610139804b40cc2e3b73dcfe78f759f97b
maswirasuria <maswirasuria@gmail.com>
2022-08-03 09:45:38 +0800

    modified:   constants/colors.js

--- constants/colors.js
+++ constants/colors.js
@@ -2,7 +2,7 @@ const tintColor = '#2f95dc';
 
 export default {
   tintColor,
-  tabIconDefault: '#0551ED',
+  tabIconDefault: '#00266B',
   tabIconSelected: tintColor,
   tabBar: '#fefefe',
   errorBackground: 'red',
