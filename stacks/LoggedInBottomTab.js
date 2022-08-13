b8631ef9e16329eeaf4a1b81be4939ab2c54e21b
maswirasuria <maswirasuria@gmail.com>
2022-08-13 12:11:35 +0800

    Minor Edit Tab Bottom

--- stacks/LoggedInBottomTab.js
+++ stacks/LoggedInBottomTab.js
@@ -14,6 +14,28 @@ const Tab = createBottomTabNavigator();
 export default function LoggedInBottomTab({navigation}) {
   return (
     <Tab.Navigator>
+
+    screenOptions={({ route }) => ({
+   
+    tabBarActiveTintColor: "",
+    tabBarInactiveTintColor: "",
+    tabBarStyle: {
+    backgroundColor: isDark ? "#00266B" : "#FFFFFF",
+    },
+    tabBarIcon: ({ color, size }) => {
+    let iconName;
+
+    if (route.name === "My Trips") {
+      iconName = "aircraft";
+     } else if (route.name === "Account") {
+      iconName = "account-circle";
+     }
+     // You can return any component that you like here!
+     return <Entypo name={iconName} size={size} color={color} />;
+            <MaterialCommunityIcons name={iconName} color={color} size={size} />;
+    },
+   })}
+
       <Tab.Screen name="My Trips" component={MyTripsStack} options={{ headerShown: false }}  />
       <Tab.Screen name="Account" component={Account} options={{ headerShown: false }}/>
     </Tab.Navigator>
