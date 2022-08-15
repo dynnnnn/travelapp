a28a1ed3e883cb9ffa00d5c1bc8bd78ad6a3e1fe
maswirasuria <maswirasuria@gmail.com>
2022-08-15 09:53:51 +0800

    Styles in Add Trips

--- screens/AddTrip.js
+++ screens/AddTrip.js
@@ -131,5 +131,12 @@ export default function AddTrip({ navigation, route }) {
 const styles = StyleSheet.create({
   container: {
     padding: 100,
+    flex: 1,
   },
+
+  text:{
+    fontsize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'BandaNova',
+  }
 });
